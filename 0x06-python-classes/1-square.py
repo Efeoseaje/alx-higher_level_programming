523f409796d5915776e536d4b55a8887d93c494b
Efeoseaje <efeoseaje@yahoo.com>
2023-04-02 23:19:39 +0100

    Class instantiation

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
-"""Empty class Square that defines a square"""
+"""class Square that defines a square"""
 
 
 class Square:
-    """Defines a square"""
+    """Defines a squarewith private instance attribute :size """
     def __init__(self, size="0"):
         self.__size = size
