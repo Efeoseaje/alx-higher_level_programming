7e542eff4f1b4159c74e0eac3c4fef1130374f67
Efeoseaje <efeoseaje@yahoo.com>
2023-04-03 12:17:40 +0100

    Simple Rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -9,28 +9,28 @@ class Rectangle:
         self.width = width
         self.height = height
 
-    """To retrieve the width"""
     @property
     def width(self):
+        """To retrieve the width"""
         return self.__width
 
-    """To set the correct value of width"""
     @width.setter
     def width(self, value):
+        """To set the correct value of width"""
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
-    """To retrieve the height"""
     @property
     def height(self):
+        """To retrieve the height"""
         return self.__height
 
-    """To set the correct value of height"""
     @width.setter
     def height(self, value):
+        """To set the correct value of height"""
         if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
