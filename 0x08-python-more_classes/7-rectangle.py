ac0a8155fe8c85c3ccc7a722f34310a36ccf76a2
Efeoseaje <efeoseaje@yahoo.com>
2023-04-07 00:27:55 +0100

    Change representation

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -60,7 +60,7 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return ("")
         for i in range(self.__height):
-            rectangle += str(self.print_symbol * self.__width)
+            rectangle += str(self.print_symbol) * (self.__width)
             if i < self.__height - 1:
                 rectangle += "\n"  # Append a new line
         return rectangle
