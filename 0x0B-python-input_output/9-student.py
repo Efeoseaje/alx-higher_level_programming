c8f9202ba8a89bc0ffbadf1534d81aa079453731
Efeoseaje <efeoseaje@yahoo.com>
2023-04-13 22:23:28 +0100

    Student to JSON

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -4,10 +4,16 @@
 
 
 class Student:
-    """ A class that defines a student by first name, last nema and age"""
+    """ A class that defines a student by first name, last name and age"""
 
     def __init__(self, first_name, last_name, age):
-        """ Instantiation of a Student"""
+        """ Instantiation of a Student
+            
+            Args:
+                first_name: First name of the student
+                last_name: Last name of the student
+                age: Age of the student
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
