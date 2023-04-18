10edee3e91d1bcd3566b93bff1d943d2932c2e65
Efeoseaje <efeoseaje@yahoo.com>
2023-04-17 17:14:51 -0700

    Base class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" This module contains the base class for other classes"""
+
+
+class Base:
+    """ Represents the base class for other classes """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """ initializes a new base
+
+        Args:
+            id(int): The identity of the new base.
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
