77268ef48d179c59ed4fd71277bc58244a869182
Efeoseaje <efeoseaje@yahoo.com>
2023-06-13 23:54:51 +0100

    Passing Arguments

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/*
+a script that prints a message depending of the number of arguments passed
+*/
+const { argv } = require('process');
+
+if (argv.length === 2) {
+	console.log('No argument');
+} else if (argv.length > 2) {
+	console.log('Arguments found');
+}
