797800270556382a04c8b9bd2184a4e54711f7ab
Efeoseaje <efeoseaje@yahoo.com>
2023-06-14 00:11:50 +0100

    passing arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -6,6 +6,8 @@ const { argv } = require('process');
 
 if (argv.length === 2) {
   console.log('No argument');
-} else if (argv.length > 2) {
+} else if (argv.length === 3) {
+  console.log('Arguments found');
+} else {
   console.log('Arguments found');
 }
