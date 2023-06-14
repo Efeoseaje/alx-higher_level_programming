396411d1120beb2ff204ed671ee5116eb59bcf15
Efeoseaje <efeoseaje@yahoo.com>
2023-06-14 18:11:14 +0100

    Array and loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+/*
+Script that prints 3 lines using array and loop
+*/
+const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (const i of arr) {
+  console.log(i);
+}
