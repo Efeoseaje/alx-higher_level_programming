2512446ffcc4a93c8dccda30eab4104aaa09a47d
Efeoseaje <efeoseaje@yahoo.com>
2023-06-14 19:38:36 +0100

    Function that adds two integers

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+/*
+A function that prints the addition of two integers
+ */
+const a = Number(process.argv[2]);
+const b = Number(process.argv[3]);
+
+function add (a, b) {
+  return (a + b);
+}
+console.log(add(a, b));
