7c34ea3d44f4da3ae3936a0ae32f6dfae011d81c
Efeoseaje <efeoseaje@yahoo.com>
2023-06-14 18:48:57 +0100

    prints x times

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+/*
+A script that prints x times “C is fun”
+ */
+
+if (isNaN(process.argv[2]) || process.argv[2] === undefined) {
+  console.log('Missing number of occurrences');
+} else {
+		const x = process.argv[2];
+		let i = 0;
+		while (i < x) {
+			console.log('C is fun');
+			i++;
+		}
+}
